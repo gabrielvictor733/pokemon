ec0cc9fc9ab038da3a285031e798dd8a0a94bacd
gabrielvictor733 <143092384+gabrielvictor733@users.noreply.github.com>
2024-08-23 17:34:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function pokeBusca(){
-    let nome = document.querySelector('#nome').value
+    let nome = document.querySelector('#nome').value;
     fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
-    .then(response=>response.json())
-    .then(dados=>{
-        console.log(dados.name)
-        document.querySelector('#pokeNome').innerHTML=dados.name
-        console.log(dados.id)
-        document.querySelector('#pokeID').innerHTML=dados.id
-        console.log(dados.sprites.other.dream_world.front_default)
-        document.querySelector('#pokeImg').src=dados.sprites.front_default
+    .then(response => response.json())
+    .then(dados => {
+        console.log(dados.name);
+        document.querySelector('#pokeNome').innerHTML = "Nome: " + dados.name.charAt(0).toUpperCase() + dados.name.slice(1);
+        console.log(dados.id);
+        document.querySelector('#pokeID').innerHTML = "ID: " + dados.id;
+        console.log(dados.sprites.versions['generation-v']['black-white'].animated.front_default);
+        document.querySelector('#pokeImg').src = dados.sprites.versions['generation-v']['black-white'].animated.front_default;
     })
-    .catch(error=>console.log(error))
-
+    .catch(error => console.log(error));
 }
