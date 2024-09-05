14016592784cdd50be3544547ca10dc95f574771
gabrielvictor733 <143092384+gabrielvictor733@users.noreply.github.com>
2024-09-05 10:34:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function pokeBusca(){
-    let nome = document.querySelector('#nome').value;
+function pokeBusca() {
+    let nome = document.querySelector('#nome').value.toLowerCase();
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${nome}`)
-    .then(response => response.json())
-    .then(dados => {
-        console.log(dados.name);
-        document.querySelector('#pokeNome').innerHTML = "Nome: " + dados.name.charAt(0).toUpperCase() + dados.name.slice(1);
-        console.log(dados.id);
-        document.querySelector('#pokeID').innerHTML = "ID: " + dados.id;
-        console.log(dados.sprites.versions['generation-v']['black-white'].animated.front_default);
-        document.querySelector('#pokeImg').src = dados.sprites.versions['generation-v']['black-white'].animated.front_default;
-    })
-    .catch(error => console.log(error));
+        .then(response => response.json())
+        .then(dados => {
+            document.querySelector('#pokeNome').innerHTML = "Nome: " + dados.name.charAt(0).toUpperCase() + dados.name.slice(1);
+            document.querySelector('#pokeID').innerHTML = "ID: " + dados.id;
+            document.querySelector('#pokeImg').src = dados.sprites.versions['generation-v']['black-white'].animated.front_default;
+        })
+        .catch(error => console.log('Erro:', error));
 }
-document.getElementById('nome').addEventListener('keypress', function(event) {
+
+document.getElementById('nome').addEventListener('keypress', function (event) {
     if (event.key === 'Enter') {
-        event.preventDefault(); 
+        event.preventDefault();
         pokeBusca();
     }
 });
+
